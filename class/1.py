0ff6816424f572945484b0e8a32f2736da01f4ea
EmilianStoyanov <emilian.stoyanov@outlook.com>
2021-06-16 14:53:48 +0300

    tasks with class

---
+++ class/1.py
@@ -0,0 +1,11 @@
+import array
+
+for name in array.__dict__:
+    print(name)
+
+"""
+
+Write a Python program to import built-in array module and
+ display the namespace of the said module. Go to the editor
+
+"""
